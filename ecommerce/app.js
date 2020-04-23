0b33fab218d22f9951a37a4f8dba10d6611584cb
devbob <devbobbobbob@gmail.com>
2020-04-23 09:56:03 +0100

    Created Product create - ability to create a new product ProductById - show product information based on the product's ID
    Added Basic product vaildation
    check all fields have data entered
    
    Check if Image is larger than 1mb a fail before it is uploaded to database

--- ecommerce/app.js
+++ ecommerce/app.js
@@ -9,17 +9,18 @@ require("dotenv").config();
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
 const categoryRoutes = require("./routes/category");
+const productRoutes = require("./routes/product");
 
 // app
 const app = express();
 
 // db
 mongoose
-  .connect(process.env.DATABASE, {
-    useNewUrlParser: true,
-    useCreateIndex: true
-  })
-  .then(() => console.log("DB Connected"));
+    .connect(process.env.DATABASE, {
+        useNewUrlParser: true,
+        useCreateIndex: true
+    })
+    .then(() => console.log("DB Connected"));
 
 // middlewares
 app.use(morgan("dev"));
@@ -31,9 +32,10 @@ app.use(expressValidator());
 app.use("/api", authRoutes);
 app.use("/api", userRoutes);
 app.use("/api", categoryRoutes);
+app.use("/api", productRoutes);
 
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
