a0fe1b718641008c1f3cfee2921e0b0d93d478c2
devbob <devbobbobbob@gmail.com>
2020-04-24 20:40:04 +0100

    Added User Signout front end compoenent clears the users localStorage information and logs user out

--- ecommerce-front/src/core/Menu.js
+++ ecommerce-front/src/core/Menu.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link, withRouter } from "react-router-dom";
+import { signout } from "../auth";
 
 const isActive = (history, path) => {
   if (history.location.pathname === path) {
@@ -36,6 +37,19 @@ const Menu = ({ history }) => (
         >
           Signup
         </Link>
+      </li>
+      <li className="nav-item">
+        <span
+          className="nav-link"
+          style={{ cursor: "pointer", color: "#ffffff" }}
+          onClick={() =>
+            signout(() => {
+              history.push("/");
+            })
+          }
+        >
+          signout
+        </span>
       </li>
     </ul>
   </div>
