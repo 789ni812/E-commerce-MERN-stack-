f3402d9e87275ba095130887007949144a8b0cc0
devbob <devbobbobbob@gmail.com>
2020-04-25 00:01:23 +0100

    Created user dashboard and admin dashboard

--- ecommerce-front/src/user/AdminDashboard.js
+++ ecommerce-front/src/user/AdminDashboard.js
@@ -3,24 +3,24 @@ import Layout from "../core/Layout";
 import { isAuthenticated } from "../auth";
 import { Link } from "react-router-dom";
 
-const Dashboard = () => {
+const AdminDashboard = () => {
   const {
     user: { _id, name, email, role }
   } = isAuthenticated();
 
-  const userLinks = () => {
+  const adminLinks = () => {
     return (
       <div className="card">
-        <h4 className="card-header"> User userLinks </h4>
+        <h4 className="card-header">Admin Links</h4>
         <ul className="list-group">
           <li className="list-group-item">
-            <Link className="nav-link" to="/cart">
-              My Cart
+            <Link className="nav-link" to="/create/category">
+              Create Category
             </Link>
           </li>
           <li className="list-group-item">
-            <Link className="nav-link" to="/profile/update">
-              Update Profile
+            <Link className="nav-link" to="/create/product">
+              Create Product
             </Link>
           </li>
         </ul>
@@ -28,28 +28,16 @@ const Dashboard = () => {
     );
   };
 
-  const userInfo = () => {
+  const adminInfo = () => {
     return (
       <div className="card mb-5">
         <h3 className="card-header">User Information</h3>
         <ul className="list-group">
-          <li className="list-group-item"> {name}</li>
-          <li className="list-group-item"> {email}</li>
+          <li className="list-group-item">{name}</li>
+          <li className="list-group-item">{email}</li>
           <li className="list-group-item">
-            {" "}
             {role === 1 ? "Admin" : "Registered User"}
           </li>
-        </ul>
-      </div>
-    );
-  };
-
-  const purchaseHistory = () => {
-    return (
-      <div className="card mb-5">
-        <h3 className="card-header">Purchase History</h3>
-        <ul className="list-group">
-          <li className="list-group-item"> name</li>
         </ul>
       </div>
     );
@@ -58,18 +46,15 @@ const Dashboard = () => {
   return (
     <Layout
       title="Dashboard"
-      description={`Hi ${name}`}
+      description={`G'day ${name}!`}
       className="container-fluid"
     >
       <div className="row">
-        <div className="col-3">{userLinks()}</div>
-        <div className="col-9">
-          {userInfo()}
-          {purchaseHistory()}
-        </div>
+        <div className="col-3">{adminLinks()}</div>
+        <div className="col-9">{adminInfo()}</div>
       </div>
     </Layout>
   );
 };
 
-export default Dashboard;
+export default AdminDashboard;
