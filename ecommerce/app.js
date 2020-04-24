5b2c23f1e1a9ee3ba34e421ae025fe68ae083a2c
devbob <devbobbobbob@gmail.com>
2020-04-24 09:31:05 +0100

    Installed cors from npm to help with cross side scritping issues that may well arise during development

--- ecommerce/app.js
+++ ecommerce/app.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 const expressValidator = require("express-validator");
 require("dotenv").config();
 // import routes
@@ -16,17 +17,18 @@ const app = express();
 
 // db
 mongoose
-    .connect(process.env.DATABASE, {
-        useNewUrlParser: true,
-        useCreateIndex: true
-    })
-    .then(() => console.log("DB Connected"));
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true
+  })
+  .then(() => console.log("DB Connected"));
 
 // middlewares
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(expressValidator());
+app.use(cors());
 
 // routes middleware
 app.use("/api", authRoutes);
@@ -37,5 +39,5 @@ app.use("/api", productRoutes);
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
