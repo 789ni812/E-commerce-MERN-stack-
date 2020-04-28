324401ed0e02098bb389e5a59e5842e596fda04f
devbob <devbobbobbob@gmail.com>
2020-04-28 13:55:19 +0100

    Created search component and single product detail page

--- ecommerce/routes/product.js
+++ ecommerce/routes/product.js
@@ -2,16 +2,17 @@ const express = require("express");
 const router = express.Router();
 
 const {
-  create,
-  productById,
-  read,
-  remove,
-  update,
-  list,
-  listRelated,
-  listCategories,
-  listBySearch,
-  photo
+    create,
+    productById,
+    read,
+    remove,
+    update,
+    list,
+    listRelated,
+    listCategories,
+    listBySearch,
+    photo,
+    listSearch
 } = require("../controllers/product");
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
@@ -19,21 +20,22 @@ const { userById } = require("../controllers/user");
 router.get("/product/:productId", read);
 router.post("/product/create/:userId", requireSignin, isAuth, isAdmin, create);
 router.delete(
-  "/product/:productId/:userId",
-  requireSignin,
-  isAuth,
-  isAdmin,
-  remove
+    "/product/:productId/:userId",
+    requireSignin,
+    isAuth,
+    isAdmin,
+    remove
 );
 router.put(
-  "/product/:productId/:userId",
-  requireSignin,
-  isAuth,
-  isAdmin,
-  update
+    "/product/:productId/:userId",
+    requireSignin,
+    isAuth,
+    isAdmin,
+    update
 );
 
 router.get("/products", list);
+router.get("/products/search", listSearch);
 router.get("/products/related/:productId", listRelated);
 router.get("/products/categories", listCategories);
 router.post("/products/by/search", listBySearch);
